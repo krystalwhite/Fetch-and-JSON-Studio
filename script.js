7765c01e62cbdf94901c647c8588329cf83c9fca
krystalwhite <krystalsnowhite@gmail.com>
2021-06-04 18:56:44 -0500

    initial attempt at studio

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    let json = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+      console.log(response);
+      response.json().then(function(json) {
+        const container = document.getElementById("container");
+        let index = 0;
+            container.addEventListener("click", function(){
+            container.innerHTML = `
+            <div class = "astronaut">
+                <div class = "bio">
+                <h3>${json[index].firstName, json[index].lastName}</h3>
+                <ul> 
+                    <li>Hours in space: ${json[index].hoursInSpace}</li>
+                    <li>Active: ${json[index].active}</li>
+                    <li>Skills: ${json[index].skills}</li>
+                </div>
+                <img class="avatar" src="images/mae-jemison.jpg">
+                </div>
+
+            `;
+            index = (index + 1) % json.length;
+            </div>    
+        });
+            
+        });
+    });
+
+  });
