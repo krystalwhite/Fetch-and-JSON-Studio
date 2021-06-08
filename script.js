72c11ebceecf9dcbb007ed7faeedd5de8f69f48f
krystalwhite <krystalsnowhite@gmail.com>
2021-06-07 19:49:04 -0500

    single astronaut

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 window.addEventListener("load", function(){
-    let json = [];
+    // let json = [];
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
       console.log(response);
       response.json().then(function(json) {
         const container = document.getElementById("container");
         let index = 0;
-            container.addEventListener("click", function(){
+        
+            // container.addEventListener("click", function(){
             container.innerHTML = `
             <div class = "astronaut">
-                <div class = "bio">
-                <h3>${json[index].firstName, json[index].lastName}</h3>
+                <div class = "bio">      
+                <h3>${json[index].firstName} ${json[index].lastName}</h3>
                 <ul> 
                     <li>Hours in space: ${json[index].hoursInSpace}</li>
                     <li>Active: ${json[index].active}</li>
                     <li>Skills: ${json[index].skills}</li>
                 </div>
-                <img class="avatar" src="images/mae-jemison.jpg">
+                <img class="avatar" src="${json[index].picture}">
                 </div>
-
-            `;
+             `;
             index = (index + 1) % json.length;
-            </div>    
-        });
+        // });
             
         });
     });
