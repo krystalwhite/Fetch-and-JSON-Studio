840ca93e7e4a2f222bbc2571377faf5847758adb
krystalwhite <krystalsnowhite@gmail.com>
2021-06-11 16:43:51 -0500

    color change to green

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-      console.log(response);
       response.json().then(function(json) {
         let container = document.getElementById("container");
         let astronautCount = 0;
@@ -8,27 +7,26 @@ window.addEventListener("load", function(){
 
         for (let i = 0; i < json.length; i++) {
           astronautCount += 1;
-          
-          let orderedList = json.sort(function(a, b) {return a.hoursInSpace - b.hoursInSpace});
-          let orderedJson = orderedList[i];
-
-          for (let j = 0; j < orderedJson.length; j++) {
-            if (orderedJson[j].active) {
-              colorChange = "active";
-            }
+          if (json[i].active) {
+            colorChange = "active";
           }
+        
+          // I can put them in order OR I can tag them green; I cannot get both to happen at the same time. 
+          // NOTE: if using the below code, need to change all the json[i] to orderedJson instead.
+          // let orderedList = json.sort(function(a, b) {return a.hoursInSpace - b.hoursInSpace});
+          // let orderedJson = orderedList[i];
 
           container.innerHTML += `
             <div class = "astronaut">    
             <div class = "bio">      
-                <h3>${orderedJson.firstName} ${orderedJson.lastName}</h3>
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
                 <ul> 
-                    <li>Hours in space: ${orderedJson.hoursInSpace}</li>
-                    <li class = ${colorChange}>Active: ${orderedJson.active}</li>
-                    <li>Skills: ${orderedJson.skills}</li>
+                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                    <li class = ${colorChange}>Active: ${json[i].active}</li>
+                    <li>Skills: ${json[i].skills}</li>
                 </ul>
                 </div>
-                <img class="avatar" src="${orderedJson.picture}">
+                <img class="avatar" src="${json[i].picture}">
                 </div>`;
                
         }
